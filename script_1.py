8e30d3b2a0c245bb820590ef69150bf2c8460687
matolgithub <89175955650@mail.ru>
2022-09-05 21:29:23 +0300

    Second commit.

--- script_1.py
+++ script_1.py
@@ -1,1 +1,29 @@
-print(f"The __name__ is: {__name__}")
+# print(f"The __name__ of script_1.py is: {__name__}")
+
+
+# if __name__ == '__main__':
+#     print(f"The __name__ of script_1.py is: {__name__}.")
+
+
+# if __name__ == "__main__":
+#     print(f"Я работаю, как независимая программа с именем {__name__}.")
+# else:
+#     print(f"Я работаю, как импортированная программа с именем {__name__}.")
+
+def say_hello(name=""):
+    print(f"Hello, {name}!")
+
+
+def set_age(age=20):
+    print(f"My age: {age} years ago.")
+
+
+def main():
+    name = input("Enter your name: ")
+    say_hello(name=name)
+    set_age(age=45)
+    print(f"Я работаю, как независимая программа с именем {__name__}.")
+
+
+if __name__ == "__main__":
+    main()
--- script_2.py
+++ script_2.py
@@ -1,3 +1,7 @@
 from script_1 import *
 
-print(f"The __name__ is: {__name__}.")
+# print(f"The __name__ of script_2.py is: {__name__}.")
+
+# set_age(age=50)
+
+say_hello(name="Ivan")
